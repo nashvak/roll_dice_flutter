8b10a31fbe141b1f537d7a3f35d1f356c7f84402
Nashva k <120083130+nashvak@users.noreply.github.com>
2023-07-26 00:17:10 +0530

    home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'diceroller.dart';
 
-const startAlignment = Alignment.topRight;
-const endAlignment = Alignment.bottomRight;
-
 class Home extends StatelessWidget {
   const Home({super.key, required this.colors});
   final List<Color> colors;
